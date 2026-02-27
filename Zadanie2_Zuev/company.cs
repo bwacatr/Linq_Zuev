52feb95db0a5ecc8fde32f3c14475e36450d7891
cheese <93201811+bwacatr@users.noreply.github.com>
2026-02-27 13:06:02 +0500

    Add files via upload

---
+++ Zadanie2_Zuev/company.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2_Zuev
+{
+    class company
+    {
+        public class Department
+        {
+            public string Name { get; set; }
+            public string Reg { get; set; }
+        }
+
+        public class Employ
+        {
+            public string Name { get; set; }
+            public string department { get; set; }
+        }
+    }
+}
